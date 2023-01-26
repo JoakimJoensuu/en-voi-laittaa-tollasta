d767858bfe9cdd0e3d11a2140e9dbaf94b71206c
Joakim Joensuu <joakim.joensuu@gmail.com>
2023-01-26 18:34:41 +0200

    Add usage&help print

--- src/main.c
+++ src/main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-int main()
+void print_usage(char *program_name)
 {
+    printf("usage: %s file_name\n", program_name);
+    printf("    file_name: path to .mpl file\n");
+    printf("\n");
+    printf("example: %s hello_world.mpl\n", program_name);
+}
+
+bool strings_are_same(char *str1, char *str2)
+{
+    if (!strcmp(str1, str2))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main(int argument_count, char *arguments[])
+{
+    if (argument_count != 2 || strings_are_same("--help", arguments[1]) || strings_are_same("-h", arguments[1]))
+    {
+        print_usage(arguments[0]);
+        return 1;
+    }
+
     printf("Hello World\n");
     return 0;
 }
